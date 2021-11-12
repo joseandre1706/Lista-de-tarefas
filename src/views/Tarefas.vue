ad2b14a3bf13bc9080e2eadc506bed0130317043
joseandre <jose.andre.psn@gmail.com>
2021-11-12 00:42:25 -0300

    adicionando as tarefas de forma dinâmica

--- src/views/Tarefas.vue
+++ src/views/Tarefas.vue
@@ -1,16 +1,29 @@
 <template>
   <div>
+    <v-col
+      cols="12"
+    >
+      <v-text-field
+        v-model="campoInput"
+        label="Qual a sua tarefa?"
+        outlined
+        clearable
+        @keyup.enter="handleAddTarefa"
+      ></v-text-field>
+    </v-col>
     <v-list
       flat
       subheader
     >
 
       <v-list-item-group
-        v-model="settings"
         multiple
         active-class=""
       >
-        <Tarefa />
+      <div v-for="tarefa, index in tarefas" :key="index">
+        <Tarefa :tarefa="tarefa"/>
+      </div>
+        
         
       </v-list-item-group>
     </v-list>
@@ -24,6 +37,25 @@ import Tarefa from '../components/tarefas/Tarefa.vue'
     name: 'Tarefas',
     components:{
       Tarefa
+    },
+    data(){
+      return{
+        campoInput:null,
+        tarefas:[
+          {titulo: "Ir ao mercado",concluido: false},
+          {titulo: "Comprar ração",concluido: false},
+          {titulo: "Fazer o almoço",concluido: false}
+
+        ]
+      }
+    },
+    methods:{
+      handleAddTarefa(){
+        if(this.campoInput){
+          this.tarefas.push({titulo: this.campoInput, concluido: false})
+          this.campoInput = null;
+        }
+      }
     }
   }
 </script>
